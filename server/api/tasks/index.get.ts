145c73092a5386d4f6b0b5d3778eb3d683aa0835
Colin Espinas <colin.espinas@yahoo.fr>
2024-02-06 16:54:05 +0100

    fix: fixed task not being filtered by user

--- server/api/tasks/index.get.ts
+++ server/api/tasks/index.get.ts
@@ -1,4 +1,5 @@
 import { eq, sql } from 'drizzle-orm'
+import { z } from 'zod'
 import { tasks } from '~/server/schema/tasks.sql'
 
 const db = useDatabase()
@@ -6,7 +7,7 @@ const allTasks = db.select().from(tasks).orderBy(tasks.id).prepare('all-tasks')
 const allTasksByUser = db.select().from(tasks).where(eq(tasks.user, sql.placeholder('user_id'))).orderBy(tasks.id).prepare('tasks-by-user')
 
 export default defineEventHandler(async (event) => {
-  const userId = getRouterParam(event, 'user')
+  const userId = await getValidatedQuery(event, (data: any) => z.string().parse(data.user))
 
   try {
     if (!userId)
