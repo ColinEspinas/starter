d5553fea7c6abf7842b2ee083962c8d5d51d780d
Colin Espinas <colin.espinas@yahoo.fr>
2024-02-21 14:00:21 +0100

    refactor: better header component

--- components/app/organisms/header.vue
+++ components/app/organisms/header.vue
@@ -4,42 +4,45 @@ const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <header v-if="$auth.loggedIn" class="flex justify-between items-center border-2 dark:border-base-900 border-base-100 p-2 rounded-xl">
+  <header class="flex justify-between items-center border-2 dark:border-base-900 border-base-100 p-2 rounded-xl">
     <div class="flex gap-2 items-center">
-      <AppMoleculesMenu />
-      <p class="font-medium">
-        {{ `${$auth.user.given_name} ${$auth.user.family_name[0]}.` }}
-      </p>
+      <template v-if="$auth.loggedIn">
+        <AppMoleculesMenu />
+        <p class="font-medium">
+          {{ `${$auth.user.given_name} ${$auth.user.family_name[0]}.` }}
+        </p>
+      </template>
+      <template v-else>
+        <p class="font-medium ml-1">
+          {{ $t('common.app-name') }}
+        </p>
+      </template>
     </div>
     <div class="flex gap-2">
-      <UiAtomsButton
-        to="/api/logout"
-        external
-        variant="accent"
-        :text="$t('common.sign-out')"
-      />
-      <UiAtomsButton :before-icon="isDark ? 'ph:moon-bold' : 'ph:sun-bold'" @click="toggleDark" />
-    </div>
-  </header>
-  <header v-else class="flex justify-between items-center border-2 dark:border-base-900 border-base-100 p-2 rounded-xl">
-    <div class="flex gap-2 items-center">
-      <p class="font-medium ml-1">
-        {{ $t('common.app-name') }}
-      </p>
-    </div>
-    <div class="flex gap-2">
-      <UiAtomsButton
-        :text="$t('common.sign-in')"
-        to="/api/login"
-        external
-      />
-      <UiAtomsButton
-        :text="$t('common.sign-up')"
-        to="/api/register"
-        variant="accent"
-        external
-      />
-      <UiAtomsButton :before-icon="isDark ? 'ph:moon-bold' : 'ph:sun-bold'" @click="toggleDark" />
+      <template v-if="$auth.loggedIn">
+        <UiAtomsButton
+          to="/api/logout"
+          external
+          variant="accent"
+          :text="$t('common.sign-out')"
+        />
+      </template>
+      <template v-else>
+        <UiAtomsButton
+          :text="$t('common.sign-in')"
+          to="/api/login"
+          external
+        />
+        <UiAtomsButton
+          :text="$t('common.sign-up')"
+          to="/api/register"
+          variant="accent"
+          external
+        />
+      </template>
+      <ClientOnly>
+        <UiAtomsButton :before-icon="isDark ? 'ph:moon-bold' : 'ph:sun-bold'" @click="toggleDark" />
+      </ClientOnly>
     </div>
   </header>
 </template>
