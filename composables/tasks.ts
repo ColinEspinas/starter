b77849815c64c1d817138688ba5b93b4c2321872
Colin Espinas <colin.espinas@yahoo.fr>
2024-02-21 11:42:24 +0100

    fix: wrong condition for tasks limitation

--- composables/tasks.ts
+++ composables/tasks.ts
@@ -19,7 +19,7 @@ export async function useTasks() {
 
   async function addTask(title: string) {
     if (loggedIn) {
-      if (!isPro && tasks.value.length <= 3)
+      if (!isPro && tasks.value.length >= 3)
         return
       try {
         const addedTask = {
