c5f97547e3a22a7b85b80939517351b0712cb5a7
Colin Espinas <colin.espinas@yahoo.fr>
2024-06-28 19:42:44 +0200

    feat: added debounce to task toggle

--- components/app/atoms/task.vue
+++ components/app/atoms/task.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { Task } from '~/server/schema/tasks.sql'
 
-defineProps<{
+const props = defineProps<{
   task: Task
   loading?: boolean
 }>()
@@ -10,13 +10,20 @@ const emits = defineEmits<{
   'delete': []
   'update:completed': [value: boolean]
 }>()
+
+const completed = ref(props.task.completed)
+const completedDebounced = refDebounced(completed, 500)
+
+watch(completedDebounced, (value) => {
+  emits('update:completed', value)
+})
 </script>
 
 <template>
   <li class="flex  bg-base-100 dark:bg-base-900 rounded-xl pl-3 p-2">
     <label class="group w-full cursor-pointer flex flex-row gap-2 items-center">
       <CheckboxRoot
-        :checked="task.completed"
+        :checked="completed"
         class="
           transition-all
           flex shrink-0 h-[24px] w-[24px] appearance-none outline-none
@@ -25,7 +32,7 @@ const emits = defineEmits<{
           border-2 border-base-200 dark:border-base-800
           group-focus-within:dark:border-base-400 group-focus-within:border-base-600
         "
-        @update:checked="emits('update:completed', $event)"
+        @update:checked="completed = $event"
       >
         <CheckboxIndicator class="shrink-0 h-full w-full rounded-md flex items-center justify-center">
           <Icon name="ph:check-bold" class="shrink-0 h-4 w-4 text-accent-800 dark:text-accent-400" />
